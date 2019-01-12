744d4b1bb0ed6547d0d99342bc1b142be7bf9949
DiodeRed <DiodeRed@users.noreply.github.com>
2019-01-12 01:03:01 -0700

    Initial commit of packages

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+set -e
+THISPATH="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
+cd "$THISPATH"
+
+function updatePkg() {
+	cd "${THISPATH}/${1}"
+	makepkg-mingw -s -i --noconfirm
+	makepkg-mingw --allsource || true
+	cd "$THISPATH"
+}
+
+updatePkg icestorm
+updatePkg nextpnr-ice40
+
+updatePkg iverilog
+updatePkg yosys
+
+updatePkg migen
+
+updatePkg verilator
